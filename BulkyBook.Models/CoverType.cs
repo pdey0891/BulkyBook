c6415f0ac3c861a0cd590b9f822b94390e66c40e
Dey <priydey@deloitte.com>
2021-02-27 19:36:46 +0530

    Add project files.

---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BulkyBook.Models
+{
+   public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name ="Cover Type")]
+        public string Name { get; set; }
+
+    }
+}
